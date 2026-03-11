2742c74514d42b6ee4759df5cb2ff60f4f9bbf31
devesh-386 <deveshsv.386@gmail.com>
2026-03-11 09:18:27 +0530

    Add Main class for Palindrome Checker App
    Initial implementation of the Palindrome Checker application with a welcome message and application details.

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // UC1: Application Entry & Welcome Message
+        System.out.println("=================================");
+        System.out.println(" Welcome to Palindrome Checker App ");
+        System.out.println("=================================");
+
+        System.out.println("Application Name : Palindrome Checker App");
+        System.out.println("Application Version : 1.0");
+
+        System.out.println("---------------------------------");
+        System.out.println("Application Started Successfully");
+        System.out.println("---------------------------------");
+
+    }
+}
